133709fe1c1735d4ccb018b06449a82f7a26a06f
Victor Sergeev <38391733+Essvitex@users.noreply.github.com>
2018-04-17 20:16:35 +0500

    Main file

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using FirstLab.Commands;
+using SecondLab;
+using Secondlab;
+/* Лабораторная работа №2: Операции над рациональными числами.
+ * Исполнители: Зотов Антон, Сергеев Виктор.
+ */
+namespace FirstLab
+{
+    class Program
+    {
+        private static void Main(string[] args)
+        {
+            var robot = new Processing();
+            robot.AddCommands(
+                new List<ICommand>
+            {
+                new AddCommand(),
+                new DivCommand(),
+                new MulCommand(),
+                new SubCommand()
+            });
+            robot.Run();
+
+        }
+    }
+}
