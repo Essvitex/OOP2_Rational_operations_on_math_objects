6e84893f0e561cfeff2014d1e52183f8aeef7723
Victor Sergeev <38391733+Essvitex@users.noreply.github.com>
2018-04-17 20:17:38 +0500

    Tokens

---
+++ Token.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Generic;
+using Secondlab;
+
+namespace FirstLab
+{
+    /// <summary>
+    /// Команда в удобном для исполнителя виде
+    /// </summary>
+    public class Token
+    {
+        /// <summary>
+        /// Название команды
+        /// </summary>
+        public string Name { get; }
+
+        /// <summary>
+        /// Параметры команды
+        /// </summary>
+        public List<string> Param { get; }
+
+        public Token(string name, List<string> param)
+        {
+            Name = name;
+            Param = param;
+        }
+    }
+}
---
+++ Tokenizer.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Secondlab;
+
+namespace FirstLab
+{
+    public static class Tokenizer
+    {
+        public static Token GetToken(string str)
+        {
+            try
+            {
+                if (string.IsNullOrEmpty(str) || string.IsNullOrWhiteSpace(str))
+                    return null;
+                var currListComm =
+                    str.Split(new[] {' '}, StringSplitOptions.RemoveEmptyEntries).ToList();
+                var commandName = currListComm.First();
+                currListComm.RemoveAt(0);
+                
+                return new Token(commandName, currListComm);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+            }
+            return null;
+        }
+    }
+}
